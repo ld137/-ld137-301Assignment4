b9c0e8d6ab9c466243c6d2092a265e25f479bbbf
ld137 <83894641+ld137@users.noreply.github.com>
2023-06-10 01:14:19 +1200

    Add files via upload
    Basic boiler plate. Going to add some more tomorrow after 5 when I get back

---
+++ FSM.java
@@ -0,0 +1,10 @@
+public class FSM {
+    // Fields (variables)
+    private int[] Stack;
+    private int[] Queue;
+
+    // Constructor
+    public FSM() {
+    }
+
+}
--- REcompile.java
+++ REcompile.java
@@ -1,1 +1,8 @@
 
+public class REcompile {
+    public static void main(String[] args) {
+
+        String expression = args[0];
+    }
+}
+
